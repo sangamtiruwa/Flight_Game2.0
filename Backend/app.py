938b3733d18e9acbdac217a8fbd783628b371e9f
sangamtiruwa <145536188+sangamtiruwa@users.noreply.github.com>
2023-12-14 18:33:09 +0200

    Add files via upload

---
+++ Backend/app.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+import mysql.connector
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_cors import CORS
+
+import config
+from game import Game
+
+load_dotenv()
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+config.conn = mysql.connector.connect(
+         host='127.0.0.1',
+         port=3306,
+         database='flight_game',
+         user='root',
+         password='3790',
+         autocommit=True
+         )
+
+def fly(id, dest, consumption=0, player=None):
+    if id==0:
+        game = Game(0, dest, consumption, player)
+    else:
+        game = Game(id, dest, consumption)
+    game.location[0].fetchWeather(game)
+    nearby = game.location[0].find_nearby_airports()
+    for a in nearby:
+        game.location.append(a)
+    json_data = json.dumps(game, default=lambda o: o.__dict__, indent=4)
+    return json_data
+
+
+# http://127.0.0.1:5000/flyto?game=fEC7n0loeL95awIxgY7M&dest=EFHK&consumption=123
+@app.route('/flyto')
+def flyto():
+    args = request.args
+    id = args.get("game")
+    dest = args.get("dest")
+    consumption = args.get("consumption")
+    json_data = fly(id, dest, consumption)
+    print("*** Called flyto endpoint ***")
+    return json_data
+
+
+# http://127.0.0.1:5000/newgame?player=Vesa&loc=EFHK
+@app.route('/newgame')
+def newgame():
+    args = request.args
+    player = args.get("player")
+    dest = args.get("loc")
+    json_data = fly(0, dest, 0, player)
+    return json_data
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
